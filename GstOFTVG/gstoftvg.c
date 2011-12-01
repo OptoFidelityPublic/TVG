dec17a521ae73729aa283e3422c488524bce2a17
jpa <jpa@e3a754e5-d11d-0410-8d38-ebb782a927b9>
2011-12-01 11:27:09 +0000

    Projektin pohja
    git-svn-id: https://svn.kapsi.fi/jpa/optofidelity/tvg@1025 e3a754e5-d11d-0410-8d38-ebb782a927b9

---
+++ GstOFTVG/gstoftvg.c
@@ -0,0 +1,224 @@
+/*
+ * GStreamer
+ * Copyright (C) 2006 Stefan Kost <ensonic@users.sf.net>
+ * Copyright (C) 2011  <<user@hostname.org>>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * SECTION:element-oftvg
+ *
+ * FIXME:Describe oftvg here.
+ *
+ * <refsect2>
+ * <title>Example launch line</title>
+ * |[
+ * gst-launch -v -m fakesrc ! oftvg ! fakesink silent=TRUE
+ * ]|
+ * </refsect2>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gst/gst.h>
+#include <gst/base/gstbasetransform.h>
+
+#include "gstoftvg.h"
+
+GST_DEBUG_CATEGORY_STATIC (gst_oftvg_debug);
+#define GST_CAT_DEFAULT gst_oftvg_debug
+
+/* Filter signals and args */
+enum
+{
+  /* FILL ME */
+  LAST_SIGNAL
+};
+
+enum
+{
+  PROP_0,
+  PROP_SILENT,
+};
+
+/* the capabilities of the inputs and outputs.
+ *
+ * FIXME:describe the real formats here.
+ */
+static GstStaticPadTemplate sink_template =
+GST_STATIC_PAD_TEMPLATE (
+  "sink",
+  GST_PAD_SINK,
+  GST_PAD_ALWAYS,
+  GST_STATIC_CAPS ("ANY")
+);
+
+static GstStaticPadTemplate src_template =
+GST_STATIC_PAD_TEMPLATE (
+  "src",
+  GST_PAD_SRC,
+  GST_PAD_ALWAYS,
+  GST_STATIC_CAPS ("ANY")
+);
+
+/* debug category for fltering log messages
+ *
+ * FIXME:exchange the string 'Template oftvg' with your description
+ */
+#define DEBUG_INIT(bla) \
+  GST_DEBUG_CATEGORY_INIT (gst_oftvg_debug, "oftvg", 0, "Template oftvg");
+
+GST_BOILERPLATE_FULL (GstOFTVG, gst_oftvg, GstBaseTransform,
+    GST_TYPE_BASE_TRANSFORM, DEBUG_INIT);
+
+static void gst_oftvg_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec);
+static void gst_oftvg_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec);
+
+static GstFlowReturn gst_oftvg_transform_ip (GstBaseTransform * base,
+    GstBuffer * outbuf);
+
+/* GObject vmethod implementations */
+
+static void
+gst_oftvg_base_init (gpointer klass)
+{
+  GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
+
+  gst_element_class_set_details_simple (element_class,
+    "OFTVG",
+    "Generic/Filter",
+    "FIXME:Generic Template Filter",
+    " <<user@hostname.org>>");
+
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&src_template));
+  gst_element_class_add_pad_template (element_class,
+      gst_static_pad_template_get (&sink_template));
+}
+
+/* initialize the oftvg's class */
+static void
+gst_oftvg_class_init (GstOFTVGClass * klass)
+{
+  GObjectClass *gobject_class;
+
+  gobject_class = (GObjectClass *) klass;
+  gobject_class->set_property = gst_oftvg_set_property;
+  gobject_class->get_property = gst_oftvg_get_property;
+
+  g_object_class_install_property (gobject_class, PROP_SILENT,
+    g_param_spec_boolean ("silent", "Silent", "Produce verbose output ?",
+          FALSE, (GParamFlags)(G_PARAM_READWRITE | GST_PARAM_CONTROLLABLE)));
+
+  GST_BASE_TRANSFORM_CLASS (klass)->transform_ip =
+      GST_DEBUG_FUNCPTR (gst_oftvg_transform_ip);
+}
+
+/* initialize the new element
+ * initialize instance structure
+ */
+static void
+gst_oftvg_init (GstOFTVG *filter, GstOFTVGClass * klass)
+{
+  filter->silent = FALSE;
+}
+
+static void
+gst_oftvg_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstOFTVG *filter = GST_OFTVG (object);
+
+  switch (prop_id) {
+    case PROP_SILENT:
+      filter->silent = g_value_get_boolean (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_oftvg_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstOFTVG *filter = GST_OFTVG (object);
+
+  switch (prop_id) {
+    case PROP_SILENT:
+      g_value_set_boolean (value, filter->silent);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+/* GstBaseTransform vmethod implementations */
+
+/* this function does the actual processing
+ */
+static GstFlowReturn
+gst_oftvg_transform_ip (GstBaseTransform * base, GstBuffer * outbuf)
+{
+  GstOFTVG *filter = GST_OFTVG (base);
+
+  if (filter->silent == FALSE)
+    g_print ("I'm plugged, therefore I'm in.\n");
+  
+  /* FIXME: do something interesting here.  This simply copies the source
+   * to the destination. */
+
+  return GST_FLOW_OK;
+}
+
+
+/* entry point to initialize the plug-in
+ * initialize the plug-in itself
+ * register the element factories and other features
+ */
+static gboolean
+oftvg_init (GstPlugin * oftvg)
+{
+  return gst_element_register (oftvg, "oftvg", GST_RANK_NONE,
+      GST_TYPE_OFTVG);
+}
+
+#ifndef PACKAGE
+#define PACKAGE "gstoftvg"
+#endif
+
+/* gstreamer looks for this structure to register oftvgs
+ *
+ * FIXME:exchange the string 'Template oftvg' with you oftvg description
+ */
+GST_PLUGIN_DEFINE (
+    GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    "oftvg",
+    "OptoFidelity Test Video Generator",
+    oftvg_init,
+    "Compiled " __DATE__ " " __TIME__,
+    "LGPL",
+    "OptoFidelity",
+    "http://optofidelity.com/"
+)
