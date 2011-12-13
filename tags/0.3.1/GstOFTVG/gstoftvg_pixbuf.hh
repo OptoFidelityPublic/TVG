f23fedb0b8bc9c6bfe0f5db89c794fc68807a8f0
harrij <harrij@e3a754e5-d11d-0410-8d38-ebb782a927b9>
2011-12-13 15:14:03 +0000

    Tagged 0.3.1 from trunk
    git-svn-id: https://svn.kapsi.fi/jpa/optofidelity/tvg@1067 e3a754e5-d11d-0410-8d38-ebb782a927b9

---
+++ tags/0.3.1/GstOFTVG/gstoftvg_pixbuf.hh
@@ -0,0 +1,123 @@
+/*
+ * OptoFidelity Test Video Generator
+ * Copyright (C) 2011 OptoFidelity <info@optofidelity.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+ 
+#ifndef __GSTOFTVG_PIXBUF_H__
+#define __GSTOFTVG_PIXBUF_H__
+
+#include <vector>
+
+#include <gst/gst.h>
+#include <gst/video/video.h>
+#include <gst/base/gstbasetransform.h>
+
+G_BEGIN_DECLS
+
+class GstOFTVGLayout;
+
+/// Layout element.
+class GstOFTVGElement
+{
+public:
+  GstOFTVGElement(int x, int y, int width, int height,
+    gboolean isSyncMark, int offset, int period, int duty);
+
+  GstOFTVGElement::GstOFTVGElement(int x, int y, int width, int height,
+    gboolean isSyncMark, int frameid_n);
+
+  // Implicit copy constructor works.
+  inline const int& x() const { return x_; }
+  inline const int& y() const { return y_; }
+  inline const int& width() const { return width_; }
+  inline const int& height() const { return height_; }
+
+  inline gboolean isBitOn(int frame_number) const
+  {
+    return ((frame_number + (period_ - offset_)) % period_) >= duty_;
+  }
+
+  /// Returns whether the properties apart from location and
+  /// size equal to element b.
+  gboolean propertiesEqual(const GstOFTVGElement& b) const;
+private:
+  int x_;
+  int y_;
+  int width_;
+  int height_;
+
+  int offset_;
+  int period_;
+  int duty_;
+
+  /// Whether this element is sync id element which will not be paused.
+  gboolean isSyncMark_;
+
+  friend class GstOFTVGLayout;
+};
+
+class GstOFTVGLayout
+{
+public:
+  /// Constructs a new layout object.
+  GstOFTVGLayout();
+
+  /// Clears the layout.
+  void clear();
+
+  /// Adds an element to the layout.
+  void addElement(const GstOFTVGElement& element);
+  
+  /// Adds a pixel to the layout.
+  void addPixel(int x, int y, int frameid_n, gboolean isSyncMark);
+  
+  /// Returns the number of elements.
+  int length() const;
+
+  /// Returns the list of elements.
+  const GstOFTVGElement* elements() const;
+
+  /// Returns the number the highest frame number that can be
+  /// represented by the frame id marks in the layout.
+  int maxFrameNumber() const;
+
+protected:
+  /// Get last element.
+  /// Precondition: length() > 0.
+  GstOFTVGElement& last();
+
+private:
+  std::vector<GstOFTVGElement> elements_;
+  int frameidBits_;
+};
+
+/**
+ * Loads layout from a bitmap file.
+ * If there is an error, error will point to the error message.
+ * @param filename name and path of the file
+ * @param error Pointer will be set to the error message if there is an error.
+ * @param layout Pointer to layout.
+ * @param width The target width of the layout.
+ * @param height The target height of the layout.
+ */
+void gst_oftvg_load_layout_bitmap(const gchar* filename, GError **error,
+  GstOFTVGLayout* layout, int width, int height);
+
+G_END_DECLS
+
+#endif /* __GST_OFTVG_H__ */
---
+++ tags/0.3.1/GstOFTVG/timemeasure.h
@@ -0,0 +1,78 @@
+#ifndef _VL_TIMEMEASURE_H_
+#define _VL_TIMEMEASURE_H_
+
+#include <stdio.h>
+
+#ifdef _WIN32
+#pragma warning(push, 0)
+#include <windows.h>
+#pragma warning(pop)
+#else
+#include <sys/time.h>
+#endif
+
+#ifdef DO_TIMING
+#ifdef _WIN32
+typedef LONGLONG timemeasure_t;
+#else
+typedef struct timeval timemeasure_t;
+#endif
+#else
+typedef int timemeasure_t;
+#endif
+
+static inline timemeasure_t begin_timing()
+{
+#ifdef DO_TIMING
+#ifdef _WIN32
+    LONGLONG result;
+    QueryPerformanceCounter((PLARGE_INTEGER)&result);
+    return result;
+#else
+    struct timeval result;
+    gettimeofday(&result, NULL);
+    return result;
+#endif
+#else
+    return 0;
+#endif
+}
+
+static inline double end_timing(timemeasure_t timer, const char *name)
+{
+    double result = 0.0;
+
+#ifdef DO_TIMING
+#ifdef _WIN32
+    LONGLONG start = timer;
+    LONGLONG end = begin_timing();
+    LONGLONG ticks_per_sec;
+    QueryPerformanceFrequency((PLARGE_INTEGER)&ticks_per_sec);
+    result = (double)(end - start) / ticks_per_sec;
+#else
+    timemeasure_t end = begin_timing();
+    end.tv_sec -= timer.tv_sec;
+    end.tv_usec -= timer.tv_usec;
+    
+    if (end.tv_usec < 0)
+    {
+        end.tv_usec += 1000000;
+        end.tv_sec -= 1;
+    }
+    
+    result = end.tv_sec + end.tv_usec / 1000000.0;
+#endif
+    
+    //fprintf(stderr, "Timer %15s: %0.6f\n", name, result);
+#endif
+
+    return result;
+}
+
+static void show_timing(double result, const char* name)
+{
+  g_print("Timer %15s: %0.6f (%0.2f / s)\n",
+    name, result, result>0.000001?1.0/result:0.0);
+}
+
+#endif
