1e25a6ed13d3017abab302b77ca9b549e655c474
Petteri Aimonen <jpa@git.mail.kapsi.fi>
2017-06-08 15:14:02 +0300

    Fix warnings about gst-plugin-scanner

--- distribution/gst-env.bat
+++ distribution/gst-env.bat
@@ -2,6 +2,7 @@ set GSTREAMER_DIR=%~dp0
 set PATH=%GSTREAMER_DIR%bin;%PATH%
 set GST_PLUGIN_PATH=
 set GST_PLUGIN_SYSTEM_PATH=%GSTREAMER_DIR%lib\gstreamer-1.0
+set GST_PLUGIN_SCANNER=%GSTREAMER_DIR%\bin\gst-plugin-scanner
 set GST_REGISTRY=%GSTREAMER_DIR%lib\registry.bin
 
 
